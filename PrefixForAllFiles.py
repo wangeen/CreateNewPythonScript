d81c405d5f49170ba88da472994518e9e9b74dac
jwang <mac@macmatoMacBook-Air.local>
2014-05-04 06:36:15 +0800

    temporary version

--- PrefixForAllFiles.py
+++ PrefixForAllFiles.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import subprocess
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m",  "--message",  help="this is sample python script", action='store_true')
+parser.add_argument("-p",  "--prefix",  help="prefix string")
+parser.add_argument("-f",  "--file",  help="file name with wildcard")
 args = parser.parse_args()
 
 def script(cmd):
@@ -16,5 +19,14 @@ if __name__  == "__main__":
         print "start script"
         ## TODO,  to be added here
         print "end script"
+    elif args.file and args.prefix:
+        print "xxx"
+        print args.file
+        print args.prefix
+        print "xxx"
+        files = glob.glob(args.file)
+        for file in files:
+            script("mv {0} {1}{0}".format(file, args.prefix))
+        pass
     else:
         print "please input --help for more detail"
