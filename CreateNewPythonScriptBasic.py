e85380e305091a3543b85cbd55ac979bd2ebf863
wangeen <wangeen@gmail.com>
2014-06-11 16:15:51 +0800

    argparse is better in main

--- CreateNewPythonScriptBasic.py
+++ CreateNewPythonScriptBasic.py
@@ -2,9 +2,6 @@
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m",  "--message",  help="this is sample python script", action='store_true')
-args = parser.parse_args()
 
 def script(cmd):
     print cmd
@@ -12,6 +9,9 @@ def script(cmd):
 
 
 if __name__  == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m",  "--message",  help="this is sample python script", action='store_true')
+    args = parser.parse_args()
     if args.message:
         print "start script"
         ## TODO,  to be added here
